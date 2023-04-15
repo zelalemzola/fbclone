c32d99ec1ea50f35c9541fe946ada41b210f024b
zola <tzelalemtesfaye@gmail.com>
2023-04-15 19:36:27 +0300

    added functionality

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var settingsMenu = document.querySelector(".settings-menu")
+var darkbtn= document.getElementById("dark-btn")
 function settingsMenuToggle()
 {
       settingsMenu.classList.toggle("settings-menu-height")
 }
+ darkbtn.onclick = function () {
+    darkbtn.classList.toggle("dark-btn-on")
+    document.body.classList.toggle("dark-theme")
+ }
